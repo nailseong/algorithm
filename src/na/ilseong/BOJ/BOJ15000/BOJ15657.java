7b46985070f1741fb1c1dd9c6446c9cd980af2bf
nailseong <nnis9857@gmail.com>
2022-06-29 21:36:24 +0900

    백준 15657

---
+++ src/na/ilseong/BOJ/BOJ15000/BOJ15657.java
@@ -0,0 +1,57 @@
+package na.ilseong.BOJ.BOJ15000;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ15657 {
+
+    private static BufferedWriter bw;
+
+    private static int N;
+    private static int M;
+    private static int[] arr;
+
+    private static int[] series;
+
+    public static void main(String[] args) throws Exception {
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        series = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        solve(0, 0);
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static void solve(int preIdx, int depth) throws Exception {
+        if (depth == M) {
+            print();
+            return;
+        }
+        for (int i = preIdx; i < N; i++) {
+            series[depth] = arr[i];
+            solve(i, depth + 1);
+            series[depth] = 0;
+        }
+    }
+
+    private static void print() throws Exception {
+        for (int n : series) {
+            bw.write(n + " ");
+        }
+        bw.write("\n");
+    }
+}
